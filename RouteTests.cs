77fc10ae6a366a9fed9edb40e1a6ac63371ea3c0
Weshiwess <150402185+WeslyQuilendireno@users.noreply.github.com>
2026-06-07 15:44:12 +0800

    add integration tests for core guest routes

---
+++ RouteTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.Net;
+using Xunit;
+
+namespace StayWithMeh.Tests
+{
+    public class RouteTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+
+        public RouteTests(WebApplicationFactory<Program> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Theory]
+        [InlineData("/")]
+        [InlineData("/Explore")]
+        [InlineData("/Booking/MyBookings")]
+        [InlineData("/Room")]
+        [InlineData("/Wishlist")]
+        [InlineData("/Support")]
+        public async Task Pages_Return200(string url)
+        {
+            var response = await _client.GetAsync(url);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
