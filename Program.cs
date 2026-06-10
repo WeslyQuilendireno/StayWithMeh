e7a56c40ee1beadfcccceacc3f18e395bfdc5cb1
Weshiwes <k3thprop@gmail.com>
2026-06-10 14:40:44 +0800

    program.cs - enable Razor runtime compilation for live cshtml reloading

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+    .AddRazorRuntimeCompilation(); // enables live .cshtml reload on save
 
 var app = builder.Build();
 
@@ -9,15 +10,12 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
