5f85cd048b4b306bc5b471aedab3c4694540e4a3
Weshiwes <k3thprop@gmail.com>
2026-06-13 08:48:51 +0800

    add ExploreViewModel to bundle Properties and Rooms -- Carries List<Property> and List<Room> to Explore/Index view

---
+++ Models/ExploreViewModel.cs
@@ -0,0 +1,8 @@
+namespace StayWithMeh.Models
+{
+    public class ExploreViewModel
+    {
+        public List<Property> Properties { get; set; } = new();
+        public List<Room> Rooms { get; set; } = new();
+    }
+}
