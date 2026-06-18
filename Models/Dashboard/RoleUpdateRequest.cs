cbceac7e4babc804a397663af15ab01a8ed6bbb6
Weshiwes <k3thprop@gmail.com>
2026-06-18 11:13:36 +0800

    add RoleUpdateRequest DTO

---
+++ Models/Dashboard/RoleUpdateRequest.cs
@@ -0,0 +1,8 @@
+namespace StayWithMeh.Models
+{
+    public class RoleUpdateRequest
+    {
+        public string UserId { get; set; } = string.Empty;
+        public string NewRole { get; set; } = string.Empty;
+    }
+}
